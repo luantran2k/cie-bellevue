995004c549db569cf2c36a7cf51a9e4f8f432b4c
luantran2k <tranluan2kyl@gmail.com>
2022-08-06 14:31:49 +0700

    complete view register detail

--- js/registerDetail.js
+++ js/registerDetail.js
@@ -1,4 +1,25 @@
 import { readResigerById } from "./handleForm.js";
-const id = localStorage.getItem("register-id");
-console.log(id);
-readResigerById(id).then((data) => console.log(data));
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+function start() {
+    const id = localStorage.getItem("register-id");
+    readResigerById(id).then((registerObj) => {
+        fillData(registerObj);
+    });
+}
+
+function fillData(registerObj) {
+    document.title = registerObj.name;
+    $(".profile-picture img").src = registerObj.portrait;
+
+    [...$$(".info-item span")].forEach((item) => {
+        item.innerText = registerObj[item.parentNode.id];
+    });
+
+    [...$$(".document-item a")].forEach((item) => {
+        item.href = registerObj[item.id];
+    });
+}
+
+start();
